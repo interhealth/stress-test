cd7cac31bfb4751eddde89dddf701e353b0efe65
interhealth <gramen.tontchev@intersystems.com>
2019-09-23 14:24:56 -0400

    get all IRIS files from /nethome/synthea

--- loadSynthea.sh
+++ loadSynthea.sh
@@ -1,6 +1,8 @@
+unzip /nethome/synthea/QDHS.zip
+cwd=$(pwd)
 iris session STRESS <<- EOS
 _system
 SYS
-zn "HSLIB" d \$SYSTEM.OBJ.Load("/home/synthea/Synthea.xml","cfk")
+zn "HSLIB" d \$SYSTEM.OBJ.LoadDir("$cwd/QDHS/","*.xml","cfk") 
 EOS
 
